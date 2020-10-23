b921bace5aa70cb0953864ca645b0008dbb5d9aa
georgy-kirilov <georgynk17@gmail.com>
2020-10-23 12:11:24 +0300

    TestMethod annotation access modifier was changed to public

--- src/models/TestMethod.java
+++ src/models/TestMethod.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-@interface TestMethod
+
+public @interface TestMethod
 {
 }
