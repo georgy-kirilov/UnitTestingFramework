c24f00626f0b39f8a8ccd47911a0c40ec150d2d2
georgy-kirilov <georgynk17@gmail.com>
2020-10-23 12:09:33 +0300

    Added toString method to the TestStatus enum. Added descriptionfield and toString method to the TestResult class

--- src/enums/TestStatus.java
+++ src/enums/TestStatus.java
@@ -2,6 +2,26 @@ package enums;
 
 public enum TestStatus 
 {
-	Failed,
-	Passed
+	Failed(0),
+	Passed(1);
+	
+	private final int value;
+	
+	private TestStatus(int value)
+	{
+		this.value = value;
+	}
+	
+	public String toString()
+	{
+		switch (this.value)
+		{
+			case 0: 
+				return "Failed";
+			case 1:
+				return "Passed"; 
+		}
+		
+		throw new RuntimeException("Invalid TestStatus enum value");
+	}
 }
--- src/models/TestResult.java
+++ src/models/TestResult.java
@@ -4,11 +4,10 @@ import enums.TestStatus;
 import validation.ExceptionValidator;
 
 public class TestResult 
-{
-	// TODO: add description field 
-	
+{	
 	private String testName;
 	private TestStatus status;
+	private String description;
 	
 	public TestResult(String testName, TestStatus status)
 	{
@@ -16,9 +15,25 @@ public class TestResult
 		this.setStatus(status);
 	}
 	
+	public TestResult(String testName, TestStatus status, String description)
+	{
+		this(testName, status);
+		this.setDescription(description);
+	}
+	
 	public String getTestName()
 	{
 		return this.testName;
+	}
+	
+	public TestStatus getStatus()
+	{
+		return this.status;
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
 	}
 	
 	private void setTestName(String value)
@@ -27,14 +42,27 @@ public class TestResult
 		this.testName = value;
 	}
 	
-	public TestStatus getStatus()
-	{
-		return this.status;
-	}
-	
 	private void setStatus(TestStatus value)
 	{
 		ExceptionValidator.throwIfNull(value, "status");
 		this.status = value;
+	}
+	
+	private void setDescription(String value)
+	{
+		ExceptionValidator.throwIfNullOrEmpty(value, "description");
+		this.description = value;
+	}
+	
+	public String toString()
+	{	
+		String result = this.getTestName() + "() -> " + this.getStatus();
+		
+		String descriptionValue = this.getDescription() == null ? 
+										"N/A" : this.getDescription();
+		
+		result += "\n" + "description: " + descriptionValue; 
+		
+		return result;
 	}
 }
